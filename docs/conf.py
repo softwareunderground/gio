d625e31d89b7090a453234ecc1aad313d957c68a
kwinkunks <matt@agilegeoscience.com>
2022-03-02 22:21:29 -0400

    docs reorg

--- docs/conf.py
+++ docs/conf.py
@@ -71,7 +71,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'notebooks']
+exclude_patterns = ['_build', 'userguide_src']
 
 
 # -- Options for HTML output -------------------------------------------------
